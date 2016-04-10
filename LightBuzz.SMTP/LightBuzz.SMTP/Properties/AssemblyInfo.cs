b326458f1a135b405dff5d5359dfa733646ce881
PrimalZed <primal.zed@gmail.com>
2016-04-09 21:00:11 -0500

    Update AssemblyInfo.cs
    fix assembly info in AssemblyInfo.cs

--- LightBuzz.SMTP/LightBuzz.SMTP/Properties/AssemblyInfo.cs
+++ LightBuzz.SMTP/LightBuzz.SMTP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LightBuzz")]
+[assembly: AssemblyTitle("LightBuzz.SMTP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LightBuzz")]
+[assembly: AssemblyProduct("LightBuzz.SMTP")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
